d3bc7b13d5aacef49385dc927e633c9ce4089ad1
tensyokuneko <npon.fafa@gmail.com>
2025-09-22 05:58:19 +0900

    ウィンドウを画面右端に配置

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, screen } = require("electron");
 const path = require("path");
 
 let win;
 
 function createWindow() {
+  // 画面サイズを取得
+  const { width: screenWidth, height: screenHeight } = screen.getPrimaryDisplay().workAreaSize;
+  
   win = new BrowserWindow({
     width: 420,
     height: 720,
+    // 画面右端に配置
+    x: screenWidth - 420 - 20, // 右端から20px余白
+    y: 50, // 上から50px
     resizable: false,
     alwaysOnTop: true,
     webPreferences: {
